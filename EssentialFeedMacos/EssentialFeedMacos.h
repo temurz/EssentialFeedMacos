d99d75d23bc25eeb66b3fd1269bfcca3c4933d4d
Temur <timurzaitov1998@gmail.com>
2023-11-03 17:48:00 +0100

    Initial Commit

---
+++ EssentialFeedMacos/EssentialFeedMacos.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedMacos.h
+//  EssentialFeedMacos
+//
+//  Created by Temur on 03/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedMacos.
+FOUNDATION_EXPORT double EssentialFeedMacosVersionNumber;
+
+//! Project version string for EssentialFeedMacos.
+FOUNDATION_EXPORT const unsigned char EssentialFeedMacosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedMacos/PublicHeader.h>
+
+
