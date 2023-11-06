ca7327d8c4728271469781ab545b9689a58dbb9f
Temur <timurzaitov1998@gmail.com>
2023-11-06 15:53:33 +0100

    Save on successful insertion

--- EssentialFeedMacosTests/CacheFeedUseCase/CacheFeedUseCaseTests.swift
+++ EssentialFeedMacosTests/CacheFeedUseCase/CacheFeedUseCaseTests.swift
@@ -59,6 +59,10 @@ class FeedStore {
         deletionCompletions[index](nil)
     }
     
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+    
     func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
         insertions.append((items, timestamp))
         receivedMessages.append(.insert(items, timestamp))
@@ -139,6 +143,25 @@ class CacheFeedUseCaseTests: XCTestCase {
         store.completeInsertion (with: insertionError)
         wait(for: [exp], timeout: 1.0)
         XCTAssertEqual(receivedError as? NSError, insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        let exp = expectation(description: "Wait for save completion")
+        
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertNil(receivedError)
     }
     
     //MARK: - Helpers
