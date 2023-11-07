948eb6e6733c1943b42d756797f42771d4c36ec7
Temur <timurzaitov1998@gmail.com>
2023-11-07 13:49:55 +0100

    Add `LocalFeedItem` data transfer representation to decouple storage frameworks from `FeedItem` data models

--- EssentialFeedMacosTests/CacheFeedUseCase/CacheFeedUseCaseTests.swift
+++ EssentialFeedMacosTests/CacheFeedUseCase/CacheFeedUseCaseTests.swift
@@ -39,11 +39,12 @@ class CacheFeedUseCaseTests: XCTestCase {
         let timestamp = Date()
         let (sut, store) = makeSUT(currentDate: { timestamp })
         let items = [uniqueItem(), uniqueItem()]
+        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL)}
         sut.save(items) { _ in }
         
         store.completeDeletionSuccessfully()
         
-        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(localItems, timestamp)])
     }
     
     func test_save_failsOnDeletionError() {
@@ -127,14 +128,14 @@ class CacheFeedUseCaseTests: XCTestCase {
     }
     
     private class FeedStoreSpy: FeedStore {
-        var insertions = [(items: [FeedItem], timestamp: Date)]()
+        var insertions = [(items: [LocalFeedItem], timestamp: Date)]()
         
         private var deletionCompletions = [DeletionCompletion]()
         private var insertionCompletions = [InsertionCompletion]()
         
         enum RececeivedMessage: Equatable {
             case deleteCachedFeed
-            case insert([FeedItem], Date)
+            case insert([LocalFeedItem], Date)
         }
         
         private(set) var receivedMessages = [RececeivedMessage]()
@@ -160,7 +161,7 @@ class CacheFeedUseCaseTests: XCTestCase {
             insertionCompletions[index](nil)
         }
         
-        func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
             insertions.append((items, timestamp))
             receivedMessages.append(.insert(items, timestamp))
             insertionCompletions.append(completion)
