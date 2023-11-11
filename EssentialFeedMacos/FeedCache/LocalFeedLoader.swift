3d9c6f9bdbdcdde738a425b6221d97926120a59e
Temur <timurzaitov1998@gmail.com>
2023-11-11 21:14:28 +0100

    Deletes cache on retrieval error

--- EssentialFeedMacos/FeedCache/LocalFeedLoader.swift
+++ EssentialFeedMacos/FeedCache/LocalFeedLoader.swift
@@ -35,6 +35,7 @@ public final class LocalFeedLoader {
         store.retrieve { [unowned self] result in
             switch result {
             case let .failure(error):
+                self.store.deleteCachedFeed { _ in }
                 completion(.failure(error))
             case let .found(feed, timestamp) where validate(timestamp):
                 completion(.success(feed.toModels()))
