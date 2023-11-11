b9c61c412a099ca0b51b084d4c34abc15b0ec672
Temur <timurzaitov1998@gmail.com>
2023-11-11 21:25:06 +0100

    Load command deletes cache on seven days old cache

--- EssentialFeedMacos/FeedCache/LocalFeedLoader.swift
+++ EssentialFeedMacos/FeedCache/LocalFeedLoader.swift
@@ -39,7 +39,10 @@ public final class LocalFeedLoader {
                 completion(.failure(error))
             case let .found(feed, timestamp) where validate(timestamp):
                 completion(.success(feed.toModels()))
-            case .found, .empty:
+            case .found:
+                self.store.deleteCachedFeed { _ in }
+                completion(.success([]))
+            case .empty:
                 completion(.success([]))
             }
         }
