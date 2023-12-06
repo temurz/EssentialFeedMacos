1c527d584f7b9d6f72580f53c3fe03439daf7061
Temur <timurzaitov1998@gmail.com>
2023-12-06 15:00:43 +0100

    Switch the protocol which `CoreDataFeedStoreTests` was implementing from `FeedStoreFailableSpecs` to `FeedStoreSpecs`

--- EssentialFeedMacosTests/FeedCache/CoreDataFeedStoreTests.swift
+++ EssentialFeedMacosTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -7,35 +7,11 @@
 
 import XCTest
 import EssentialFeedMacos
-class CoreDataFeedStoreTests: XCTestCase, FeedStoreFailableSpecs {
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
         
         assertThatRetrieveDeliversEmptyOnEmptyCache(sut)
-    }
-    
-    func test_insert_deliversErrorOnInsertionError() {
-        
-    }
-    
-    func test_insert_hasNoSideEffectsOnInsertionError() {
-        
-    }
-    
-    func test_retrieve_deliversErrorOnRetrievalError() {
-        
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnFailure() {
-        
-    }
-    
-    func test_delete_deliversErrorOnDeletionError() {
-        
-    }
-    
-    func test_delete_hasNoSideEffectsOnDeletionError() {
-        
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
