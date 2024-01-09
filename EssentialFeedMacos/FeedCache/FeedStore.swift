8b59cb1ec2e66e673eff130a57f36aaee0124f6f
Temur <timurzaitov1998@gmail.com>
2024-01-09 19:03:26 +0100

    Simplify `CoreDataFeedStore` completion code with the new `Result` APIs

--- EssentialFeedMacos/FeedCache/FeedStore.swift
+++ EssentialFeedMacos/FeedCache/FeedStore.swift
@@ -9,11 +9,8 @@ import Foundation
 public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date) 
 
 public protocol FeedStore {
-    typealias DeletionResult = Result<Void,Error>
-    typealias DeletionCompletion = (DeletionResult) -> Void
-    
-    typealias InsertionResult = Result<Void,Error>
-    typealias InsertionCompletion = (InsertionResult) -> Void
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
     
     typealias RetrievalResult = Result<CachedFeed?,Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void
