509a8218343045a6725fb1d5b16efc7329466cbf
Temur <timurzaitov1998@gmail.com>
2024-02-24 08:45:06 +0100

    Make `RemoteFeedImageDataLoader` conform to `FeedImageDataLoader` protocol

--- EssentialFeedMacos/FeedAPI/RemoteFeedImageDataLoader.swift
+++ EssentialFeedMacos/FeedAPI/RemoteFeedImageDataLoader.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-public final class RemoteFeedImageDataLoader {
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     private let client: HTTPClient
     
     public enum Error: Swift.Error {
