e9c669c63fe7efa57d9bd9b2eb2d490f7b11c81e
Temur <timurzaitov1998@gmail.com>
2024-05-13 14:53:04 +0200

    Fix potential bug when canceling requests in UITableView didEndDisplayingCell method - This method is invoked after calling `reloadData`, so we'd be canceling requests in the wrong models or crash in case the model has less items than the previous model

--- EssentialFeediOS/Feed UI/Controllers/FeedViewContoller.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewContoller.swift
@@ -15,6 +15,8 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     
     @IBOutlet private(set) public var errorView: ErrorView?
     
+    private var loadingControllers = [IndexPath: FeedImageCellController]()
+    
     public var delegate: FeedViewControllerDelegate?
     
     private  var tableModel = [FeedImageCellController]() {
@@ -42,6 +44,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     public func display(_ cellControllers: [FeedImageCellController]) {
+        loadingControllers = [:]
         tableModel = cellControllers
     }
     
@@ -93,10 +96,13 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     private func cellController(at indexPath: IndexPath) -> FeedImageCellController {
-        return tableModel[indexPath.row]
+        let controller = tableModel[indexPath.row]
+        loadingControllers[indexPath] = controller
+        return controller
     }
     
     private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
-        cellController(at: indexPath).cancelLoad()
+        loadingControllers[indexPath]?.cancelLoad()
+        loadingControllers[indexPath] = nil
     }
 }
--- EssentialFeediOSTests/Helpers/FeedUIIntegrationTests+Assertions.swift
+++ EssentialFeediOSTests/Helpers/FeedUIIntegrationTests+Assertions.swift
@@ -10,6 +10,8 @@ import EssentialFeedMacos
 import XCTest
 extension FeedUIIntegrationTests {
     func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+        sut.tableView.layoutIfNeeded()
+        RunLoop.main.run(until: Date())
         guard sut.numberOfRenderedFeedImageViews() == feed.count else {
             return XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead", file: file, line: line)
         }
