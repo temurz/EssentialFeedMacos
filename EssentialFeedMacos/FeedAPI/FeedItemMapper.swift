3b4faea03e6afd366a3dcc26c34f0725db6e7d90
Temur <timurzaitov1998@gmail.com>
2024-08-26 13:14:17 +0500

    Move FeedImage mapping to FeedItemsMapper

--- EssentialFeedMacos/FeedAPI/FeedItemMapper.swift
+++ EssentialFeedMacos/FeedAPI/FeedItemMapper.swift
@@ -8,14 +8,25 @@
 import Foundation
 final class FeedItemsMapper {
     private struct Root: Decodable {
-        let items: [RemoteFeedItem]
+        private let items: [RemoteFeedItem]
+        
+        private struct RemoteFeedItem: Decodable {
+            internal let id: UUID
+            internal let description: String?
+            internal let location: String?
+            internal let image: URL
+        }
+        
+        var images: [FeedImage] {
+            items.map { FeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image) }
+        }
     }
     
     private  static var OK_200: Int {return 200}
-    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
             throw RemoteFeedLoader.Error.invalidData
         }
-        return root.items
+        return root.images
     }
 }
--- EssentialFeedMacos/Image Comments API/RemoteImageCommentsLoader.swift
+++ EssentialFeedMacos/Image Comments API/RemoteImageCommentsLoader.swift
@@ -44,9 +44,3 @@ public class RemoteImageCommentsLoader {
         }
     }
 }
-
-private extension Array where Element == RemoteFeedItem {
-    func toModels() -> [FeedImage] {
-        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)}
-    }
-}
