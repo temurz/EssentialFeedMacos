3d3c8347e5bc4b6c98643a146d55787cb000cd58
Temur <timurzaitov1998@gmail.com>
2024-09-23 16:48:44 +0200

    Duplicate FeedPresenter as LoadResourcePresenter

---
+++ EssentialFeedMacos/Shared Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,42 @@
+//
+//  LoadResourcePresenter.swift
+//  EssentialFeedMacos
+//
+//  Created by Temur on 23/09/2024.
+//
+
+import Foundation
+public class LoadResourcePresenter {
+    private let loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
+    private let feedView: FeedView
+    
+    public init(errorView: FeedErrorView, loadingView: FeedLoadingView, feedView: FeedView) {
+        self.errorView = errorView
+        self.loadingView = loadingView
+        self.feedView = feedView
+    }
+    
+    public static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self), comment: "Title for the feed view")
+    }
+    
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+    
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+    
+    public func didFinishLoadingFeed(with error: Error) {
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+        errorView.display(FeedErrorViewModel(message: feedError))
+    }
+    
+    private var feedError: String {
+        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self) , comment: "Error message displayed when we cannot load the image feed from the server")
+    }
+}
